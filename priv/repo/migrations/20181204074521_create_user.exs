9764566f2fdfa8fc84fa87da677d2e003f8f7f53
zidna <zidnaziada@gmail.com>
2018-12-04 00:58:17 -0800

    interact with db

---
+++ priv/repo/migrations/20181204074521_create_user.exs
@@ -0,0 +1,15 @@
+defmodule Rumbl.Repo.Migrations.CreateUser do
+  use Ecto.Migration
+
+  def change do
+    create table(:user) do
+      add :name, :string
+      add :username, :string, null: false
+      add :password_hash, :string
+      
+      timestamps()
+    end
+
+    create unique_index(:user, [:username])
+  end
+end
