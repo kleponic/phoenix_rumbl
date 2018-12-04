41fc8c35955b6084aa57d4be3263965b8fd9d8e2
zidna <zidnaziada@gmail.com>
2018-12-03 22:25:37 -0800

    remove whitespace

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -3,12 +3,12 @@ defmodule Rumbl.Repo do
   In memory repository.
   """
   def all(Rumbl.User) do
-    [%{Rumbl.User{id:  "1", name: "Macan", username: "macan", password: "macan123"}},
-     %{Rumbl.User{id:  "2", name: "Singa", username: "singa", password: "singa123"}},
-     %{Rumbl.User{id:  "3", name: "Gajah", username: "gajah", password: "gajah123"}}]
+    [%Rumbl.User{id:  "1", name: "Macan", username: "macan", password: "macan123"},
+     %Rumbl.User{id:  "2", name: "Singa", username: "singa", password: "singa123"},
+     %Rumbl.User{id:  "3", name: "Gajah", username: "gajah", password: "gajah123"}]
   end
-
-  def all(_module), do:[]
+  
+  def all(_module), do: []
 
   def get(module, id) do 
     Enum.find all(module), fn map -> map.id == id end
@@ -16,6 +16,7 @@ defmodule Rumbl.Repo do
 
   def get_by(module, params) do
     Enum.find all(module), fn map -> 
-      Enum.all? (params, fn {key, val} -> Map.get(map, key) == val end)
+      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
+    end
   end
 end
